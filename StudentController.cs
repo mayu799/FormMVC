d8c0a222a80127d7259a669bd7228d7923329660
mayu799 <79864374+mayu799@users.noreply.github.com>
2022-03-16 12:36:23 +0530

    Add files via upload

---
+++ StudentController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using form1.Models;
+
+namespace form1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetPersonalDetails()
+        {
+            Students obj = new Students();
+
+            /*obj.FirstName = "Mayuri";
+            obj.LastName = "Gupta";
+            obj.Age = 20;
+            obj.Gender = "Female";
+            obj.City = "Gondia";*/
+
+            return View(obj);
+
+        }
+
+        public ActionResult SetPersonalDetails(string FirstName, string LastName, int Age, string Gender, string City)
+        {
+            Students obj1 = new Students();
+            obj1.FirstName = FirstName;
+            obj1.LastName = LastName;
+            obj1.Age = Age;
+            obj1.Gender = Gender;
+            obj1.City = City;
+
+            return View(obj1);
+
+        }
+    }
+}
---
+++ Students.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace form1.Models
+{
+    public class Students
+    {
+        // FirstName, LastName, Age, Gender, City
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+
+    }
+}
